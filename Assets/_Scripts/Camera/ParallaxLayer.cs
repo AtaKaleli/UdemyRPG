88dd4cbedd5230c99af7f29139b1dc6e7e954926
ata kaleli <atakaleli123@gmail.com>
2025-09-01 20:26:43 +0300

    v2.0 Camera and tiles were set

---
+++ Assets/_Scripts/Camera/ParallaxLayer.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+
+[System.Serializable]
+public class ParallaxLayer
+{
+    [SerializeField] private Transform background;
+    [SerializeField] private float parallaxMultiplier;
+}
